4383c553fc14891b1d3fa82e377d3c659253f629
Baptiste Véré <stolous@gmail.com>
2017-03-26 17:10:16 +0200

    Added position syncronization accross network

---
+++ Assets/Scripts/Player/TankCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace TankFighters.Player {
+	
+	public class TankCamera : MonoBehaviour {
+
+		public GameObject tankGameObject;
+		// Use this for initialization
+		void Start () {
+			if(!tankGameObject.GetComponent<NetworkIdentity>().hasAuthority)
+				this.gameObject.SetActive(false);
+		}
+	}
+}
--- Assets/Scripts/Player/TankMovements.cs
+++ Assets/Scripts/Player/TankMovements.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
 
-namespace TankFighters.Player {
-
-	[RequireComponent(typeof(CharacterController))]
-	public class TankMovements : MonoBehaviour {
+namespace TankFighters.Player
+{
+	[RequireComponent (typeof(CharacterController))]
+	public class TankMovements : MonoBehaviour
+	{
 
 		public int speed = 1;
 
 		private CharacterController controller;
 
-		void Start() {
+		void Start ()
+		{
 			controller = GetComponent<CharacterController>();
 			controller.enabled = true;
 		}
 
 		// Update is called once per frame
-		void Update () {
-			if(!GetComponent<NetworkIdentity>().isLocalPlayer)
+		void Update ()
+		{
+			if (!GetComponent<NetworkIdentity>().isLocalPlayer)
 				return;
 			
 			Vector3 movement = Vector3.zero;
-			movement.x += Input.GetAxis("Horizontal") * speed;
-			movement.z += Input.GetAxis("Vertical") * speed;
+			movement.x += CrossPlatformInputManager.GetAxis("Horizontal");
+			movement.z += CrossPlatformInputManager.GetAxis("Vertical");
 
 			//movement = transform.TransformDirection(movement);
 
+			movement *= speed;
 			movement += Physics.gravity;
 			movement *= Time.deltaTime;
 			Debug.Log("movement: " + movement);
