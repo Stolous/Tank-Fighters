7942f7476d9dfcedd36ab887db6a037f52008624
Baptiste Véré <stolous@gmail.com>
2017-04-11 15:48:40 +0200

    Added shooting ability

--- Assets/Scripts/Projectiles/Missile.cs
+++ Assets/Scripts/Projectiles/Missile.cs
@@ -9,9 +9,17 @@ namespace TankFighters.Projectile
 		public int damages = 50;
 		public float speed = 1f;
 
-		void FixedUpdate()
+		private Rigidbody rigidbody;
+
+		void Start()
 		{
-			transform.Translate(Vector3.up * speed);
+			this.rigidbody = GetComponent<Rigidbody>();
+			this.rigidbody.velocity = transform.forward * speed;
+		}
+
+		void LateUpdate()
+		{
+			//this.rigidbody.velocity = (speed * this.rigidbody.velocity.normalized);
 		}
 
 		void OnTriggerEnter(Collider other)
